2caede4be3417055edde3f0e7ac937c1a06574e5
ANANDPATEL2001 <anand2010098@akgec.ac.in>
2024-01-13 01:42:46 +0530

    Services Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills'
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <main className='main'>
         <Home />
         <About />
+        <Skills />
+        <Services />
       </main>
     </div>
   );
